25dd02ee712c52b52f8eb04c7806232757961964
Carlos Sola-Llonch <csolallo@gmail.com>
2026-04-11 21:06:09 -0400

    missed one class in last commit

---
+++ kexp-electron/web_app/lib/ui/now_playing/view_model.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:web_app/data/model/now_playing_track.dart';
+import 'package:web_app/data/repositories/now_playing.dart';
+import 'package:web_app/utils/result.dart';
+
+import 'dart:developer';
+
+class NowPlayingViewModel extends ChangeNotifier {
+
+  NowPlayingViewModel({
+    required NowPlayingRepository repository,
+  }) : 
+    _repository = repository;
+
+  final NowPlayingRepository _repository;
+
+  NowPlayingTrack? _currentTrack;
+  NowPlayingTrack? get currentTrack => _currentTrack;
+
+  // command methods 
+  
+  Future <void> load() async {
+    final nowPlayingResult = await _repository.fetchCurrentTrack();
+    switch (nowPlayingResult) {
+      case Ok(value: var v):
+        _currentTrack = v as NowPlayingTrack?;
+      case Err(error: var err):
+        _currentTrack = null;
+        log("Error fetching current track: $err");
+    }
+    notifyListeners();
+  }
+}
